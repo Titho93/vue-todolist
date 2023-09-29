28742f3f387b87587386cf7c27ef6a63de2740a0
titho93 <titho93.10@gmail.com>
2023-09-29 19:17:33 +0200

    [fix] index bug

--- js/script.js
+++ js/script.js
@@ -26,15 +26,18 @@ createApp({
     },
     methods:{
         addTask(){
+
             if(this.newTask.length < 3){
                 this.isError = true
             }else {
-                this.tasks.unshift(this.newTask)
+                // this.tasks.unshift(this.newTask)
+                this.tasks.unshift({ text: this.newTask, done: false });
                 this.newTask = ''
                 this.isError = false
             }
-        },
 
+        },
+    
         removeTask(index){
             this.tasks.splice(index, 1)
         }
